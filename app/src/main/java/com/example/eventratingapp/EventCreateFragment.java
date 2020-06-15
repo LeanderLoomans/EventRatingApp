db176c7e214b7fa36caa2f0ea4a9693fa2916d5a
LeanderLoomans <s1115153@student.hsleiden.nl>
2020-06-16 01:06:45 +0200

    Date en time pickers toegevoegd

--- app/src/main/java/com/example/eventratingapp/EventCreateFragment.java
+++ app/src/main/java/com/example/eventratingapp/EventCreateFragment.java
@@ -1,6 +1,10 @@
 package com.example.eventratingapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,12 +17,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.eventratingapp.database.DataBaseCommunication;
 import com.example.eventratingapp.database.MessageCallback;
 
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -31,8 +39,17 @@ public class EventCreateFragment extends Fragment {
 
     EditText nameInput;
     EditText descriptionInput;
-    EditText startDateInput;
-    EditText endDateInput;
+    TextView startDateInput;
+    TextView startTimeInput;
+    TextView endDateInput;
+    TextView endTimeInput;
+
+    DatePickerDialog.OnDateSetListener onStartDateSetListener;
+    TimePickerDialog.OnTimeSetListener onStartTimeSetListener;
+
+    DatePickerDialog.OnDateSetListener onEndDateSetListener;
+    TimePickerDialog.OnTimeSetListener onEndTimeSetListener;
+
     Button createButton;
 
     public EventCreateFragment() {
@@ -69,7 +86,67 @@ public class EventCreateFragment extends Fragment {
         nameInput = view.findViewById(R.id.nameInput);
         descriptionInput = view.findViewById(R.id.descriptionInput);
         startDateInput = view.findViewById(R.id.startDateInput);
+        startDateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeDatePickerDialog(onStartDateSetListener);
+            }
+        });
+        onStartDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month++;
+                String date = dateItemFormatter(day) + "/" + dateItemFormatter(month) + "/" + year;
+                startDateInput.setText(date);
+            }
+        };
+
+        startTimeInput = view.findViewById(R.id.startTimeInput);
+        startTimeInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeTimePickerDialog(onStartTimeSetListener);
+            }
+        });
+        onStartTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hour, int minute) {
+                String time = dateItemFormatter(hour) + ":" + dateItemFormatter(minute);
+                startTimeInput.setText(time);
+            }
+        };
+
         endDateInput = view.findViewById(R.id.endDateInput);
+        endDateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeDatePickerDialog(onEndDateSetListener);
+            }
+        });
+        onEndDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month++;
+                String date = dateItemFormatter(day) + "/" + dateItemFormatter(month) + "/" + year;
+                endDateInput.setText(date);
+            }
+        };
+
+        endTimeInput = view.findViewById(R.id.endTimeInput);
+        endTimeInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeTimePickerDialog(onEndTimeSetListener);
+            }
+        });
+        onEndTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hour, int minute) {
+                String time = dateItemFormatter(hour) + ":" + dateItemFormatter(minute);
+                endTimeInput.setText(time);
+            }
+        };
+
         createButton = view.findViewById(R.id.createButton);
         createButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -84,8 +161,10 @@ public class EventCreateFragment extends Fragment {
             DataBaseCommunication.getInstance().addNewEvent(
                     nameInput.getText().toString(),
                     descriptionInput.getText().toString(),
-                    startDateInput.getText().toString(),
-                    endDateInput.getText().toString(),
+                    startDateInput.getText().toString()
+                            + " " + startTimeInput.getText().toString(),
+                    endDateInput.getText().toString()
+                            + " " + endTimeInput.getText().toString(),
                     new MessageCallback() {
                         @Override
                         public void onCallBack(String message) {
@@ -97,6 +176,42 @@ public class EventCreateFragment extends Fragment {
         else {
             Toast.makeText(context, "Incorrecte waarden opgegeven", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void makeDatePickerDialog(DatePickerDialog.OnDateSetListener dateSetListener) {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                context,
+                android.R.style.Theme_Holo_Dialog_MinWidth,
+                dateSetListener,
+                year, month, day);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    private void makeTimePickerDialog(TimePickerDialog.OnTimeSetListener timeSetListener) {
+        Calendar cal = Calendar.getInstance();
+
+        TimePickerDialog dialog = new TimePickerDialog(
+                context,
+                android.R.style.Theme_Holo_Dialog_MinWidth,
+                timeSetListener,
+                0, 0,
+                true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    private String dateItemFormatter(int dateItem) {
+        String item = dateItem + "";
+        if (item.length() < 2) {
+            item = "0" + item;
+        }
+        return item;
     }
 
     private boolean checkFields() {
